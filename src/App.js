53ea818e63aca1d26b23fe4007bfda1cc019ac3d
Christian Ramirez <christian.r0602@gmail.com>
2022-05-05 11:16:53 -0500

    JSX todo list

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 
 
 function App() {
-  return 'Hello World'
+  return (
+    <>
+    <h1>Hello Dojo</h1>
+    <h3>Things I need to do:</h3>
+    <ul>
+      <li>Learn React</li>
+      <li>Make Lunch</li>
+      <li>Workout</li>
+      <li>Read</li>
+    </ul>
+    </>
+  )
   
 }
 
